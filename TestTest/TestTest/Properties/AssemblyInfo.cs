56fc6636322d0d0337a6a9f97af77bb8b75ec2e9
Kimlt1989 <kimlt1989@gmail.com>
2020-01-17 17:11:14 +0700

    c# notepad test

---
+++ TestTest/TestTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("83c83f6a-3408-4c4f-bb0b-1e70debbec82")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestTest/TestTest/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+
+namespace TestTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string appiumDriverURI = "http://127.0.0.1:4723";
+        private const string noteApp = @"C:\Windows\System32\notepad.exe";
+        protected static WindowsDriver<WindowsElement> NotepadSession;
+        private WindowsDriver<WindowsElement> session;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+      /*      // Launch Notepad
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", noteApp);
+            appCapabilities.SetCapability("deviceName", "WindowsPC");
+            //Create a session to intract with windows application  
+            NotepadSession = new WindowsDriver<WindowsElement>(new Uri(appiumDriverURI), appCapabilities);*/
+
+            AppiumOptions opt = new AppiumOptions();
+            opt.AddAdditionalCapability("app", noteApp);
+            opt.AddAdditionalCapability("deviceName", "WindowsPC");
+            session = new WindowsDriver<WindowsElement>(new Uri(appiumDriverURI), opt);
+
+            // Control the  app
+            session.FindElementByClassName("Edit").SendKeys("This is some text");
+
+        }
+    }
+}
